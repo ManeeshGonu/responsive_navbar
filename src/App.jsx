53215595d41ea925eb7c039178ca0b7a14ae03c6
ManeeshGonu <gonumaneesh@gmail.com>
2024-11-20 08:38:25 +0530

    Navbar Completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="min-h-[200vh] bg-slate-500"></div>
+    </div>
+  );
+};
+
+export default App;
